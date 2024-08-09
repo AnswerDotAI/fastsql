0c11d8dd99159594285bd75e1e0a672998b4c662
Pol Alvarez <pol.avms@gmail.com>
2024-08-09 16:06:02 +0200

    added pool_recycle to DB engine

--- fastsql/core.py
+++ fastsql/core.py
@@ -16,7 +16,7 @@ class Database:
     "A connection to a SQLAlchemy database"
     def __init__(self, conn_str):
         self.conn_str = conn_str
-        self.engine = sa.create_engine(conn_str, pool_pre_ping=True)
+        self.engine = sa.create_engine(conn_str, pool_pre_ping=True, pool_recycle=300)
         self.meta = sa.MetaData()
         self.meta.reflect(bind=self.engine)
         self.meta.bind = self.engine
